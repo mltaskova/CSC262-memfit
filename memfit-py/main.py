3626ab4e8e8b5ce9253b6ccb6c7daf2fef59aa27
John Foley <jfoley@cs.umass.edu>
2019-02-21 17:25:07 -0500

    python starter of some kind

---
+++ memfit-py/main.py
@@ -0,0 +1,9 @@
+class Simulation(object):
+    def __init__(self, algorithm, size):
+        self.algorithm = algorithm
+        self.size = size
+        self.free_list = []
+        self.used_list = []
+
+if __name__ == '__main__':
+    print("Hello World!");
